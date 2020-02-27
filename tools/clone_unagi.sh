b6e3ec5e7710c4ec18a0ab04dfe8f4e94ad340e6
aonori <aonori@aonr.jp>
2020-02-27 15:52:27 +0900

    Delete hajimeni.tex

--- tools/clone_unagi.sh
+++ tools/clone_unagi.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # - written by @aonrjp
 
-HTTPS_HOST='https://gitlab.com'
+GITLAB_HOST='git@gitlab.com:'
 
 # CI実行の場合はCI_JOB_TOKENを利用して取ってくる
-if [ -v CI_JOB_TOKEN ]; then
-    HTTPS_HOST="https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com"
+if [ "${CI_JOB_TOKEN}" ]; then
+    GITLAB_HOST="https://gitlab-ci-token:${CI_JOB_TOKEN}@gitlab.com/"
 fi
 
-git clone ${HTTPS_HOST}/ritscc/soukai/unagi.git
+git clone ${GITLAB_HOST}ritscc/soukai/unagi.git
 
 # 対象コミットが指定されていれば適応
 if [ -v UNAGI_COMMIT ]; then
